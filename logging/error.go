417a02804e75d9b3d79cbe7b94adb61a3b409a15
Micah Kemp <github@micah.ws>
2023-01-09 00:40:54 +0000

    Add Error

---
+++ logging/error.go
@@ -0,0 +1,9 @@
+package logging
+
+// ErrorCode indicates which type of error occurred.
+type ErrorCode int
+
+const (
+	// ErrorUndefined is an undefined error.
+	ErrorUndefined ErrorCode = iota
+)
